57b123d5e2b056b027eae867c82be820404cd8aa
Spyros Paterakis <spyridon.paterakis@outlook.com.gr>
2022-05-29 12:27:03 +0300

    Introduce error handling and did a clean up in startup

---
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,12 @@
+namespace API.Errors
+{
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public IEnumerable<string> Errors { get; set; }
+
+        public ApiValidationErrorResponse() : base(400)
+        {
+            
+        }
+    }
+}
